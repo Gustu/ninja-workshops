938d870e972e4137be56c342d282cab13cc75b2a
Bartłomiej Pasik <aproszeciebardzo@gmail.com>
2016-06-25 10:25:52 +0200

    added linter and its config

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "plugins": [
+        "node"
+    ],
+    rules: {
+      indent: [
+        'error', 2
+      ],
+      quotes: [
+        'error',
+        'single'
+      ],
+      semi: [
+        'error',
+        'allways'
+      ]
+    }
+};
